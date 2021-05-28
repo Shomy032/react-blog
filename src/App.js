82cb58df3adcc09256d740fe15fd52fe1624dd33
Milos <milosmilic032@gmail.com>
2021-05-28 06:19:27 +0200

    starting express setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React , {useState , useEffect} from 'react' ;
 import './App.css';
+// import image from './test-bl.png'
+
+
+const posts = [ 
+    {name : 'Firts post',
+  description : 'this is first post' ,
+  author : 'pera peric' ,
+  picture : '',
+  links : [],
+  social_links : []
+  },
+  { h1 : 'start',
+  p : "start",
+  img_path : './pictures/test-bl.png'} ,
+
+{p : '2. p element' ,
+h1 : 'asdasdee`11111'} ,
+{
+  img_path : './pictures/test.png',
+  p  : "end",
+  h1 : 'end'
+}
+]
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+
+  let [values , setValues] = useState([])
+   
+
+   useEffect(() => {
+
+     
+     setValues([...posts])
+   } , [])
+
+    return (
+         
+ <section>{values.map(e => (
+   <>
+  {e.h1 ? <h1>{e.h1}</h1> : null }
+  {e.p ? <p>{e.p}</p> : null }
+  {e.img_path ?
+   <img src={e.img_path}
+   alt=''
+   /> : null }
+  </>
+ ))}</section>
+        
+      )          
+
 }
 
 export default App;
